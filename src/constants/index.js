2d849db04f82ac8fa1da56fb6108ddae37910bad
sadunjayamaha <sadunjayamaha9917@gmail.com>
2024-12-09 11:58:26 +0530

    Contants folder commit

---
+++ src/constants/index.js
@@ -0,0 +1,52 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a final year Information technology undergraduate at SLIIT campus. I have a strong interest in starting my career in IT industry.I am willingness to work in adynamic and challenging
+ working environment that
+ utilize my competence and
+ skills towards the growth of
+ the organization`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2024 Jan - Present",
+    role: "Web Developer Intern",
+    company: "Eyerax Technologies Pvt Ltd",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Node.js", "mongoDB"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Hospital Staff Management Web Application",
+    image: project1,
+    description:
+      "A Web application to manage Patient records ( create, view, edit and delete) ",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Tailwind CSS"],
+  },
+];
+
+export const CONTACT = {
+  address: "196/1, St.Joseph's Street, Negombo ",
+  phoneNo: "+94 70 1723290",
+  email: "sadunjayamaha917@gmail.com",
+};
